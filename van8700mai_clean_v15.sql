81625c03c6ae35aecd3df533176e2d794cf00d19
Nikolai Nedovodin <info@cputer.com>
2025-08-26 13:55:27 -0700

    Delete van8700mai_clean_v15.sql

--- van8700mai_clean_v15.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `wp_actionscheduler_actions` (`action_id`,`hook`,`status`,`scheduled_date_gmt`,`scheduled_date_local`,`priority`,`args`,`schedule`,`group_id`,`attempts`,`last_attempt_gmt`,`last_attempt_local`,`claim_id`,`extended_args`)
-VALUES
-  (28,'woocommerce_geoip_updater','pending','2025-09-10 09:29:20',
-   '2025-09-10 09:29:20',10,'[]',
-   'O:32:"ActionScheduler_IntervalSchedule":5:{s:22:"\0*\0scheduled_timestamp";i:1757496560;s:18:"\0*\0first_timestamp";i:1756200516;s:13:"\0*\0recurrence";i:1296000;s:49:"\0ActionScheduler_IntervalSchedule\0start_timestamp";i:1757496560;s:53:"\0ActionScheduler_IntervalSchedule\0interval_in_seconds";i:1296000;}',
-   3, 0, NULL, NULL, 0, NULL);
